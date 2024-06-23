cf26f09077f4333e7764e471dd00f7d59b1bb3aa
fgh11112 <fgh11112@gmail.com>
2024-06-23 15:34:06 +0800

    addAuthenticSection

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -2,25 +2,64 @@ package com.shopme.admin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new ShopmeUserDetailsService();
+	}
 
 	@Bean
 	public PasswordEncoder PasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(PasswordEncoder());
+		
+		return authProvider;
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+	}
+	
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/login")
+				.usernameParameter("email") // spring security 預設 name 屬性為 username
+				.permitAll()
+			.and().logout().permitAll() // 讓 logout 狀態 URL 顯示
+			.and().rememberMe() // 開啟之後 cookies 多一項 remember-me
+					.key("AbcDefgHijKlmnOpqrs_1234567890") // 增加私鑰去加密 remember-me cookie
+					.tokenValiditySeconds(7 * 24 * 60 * 60);
 	}
 
-	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// 當應用程序收到對這些URL的請求時，Spring Security將不會對它們應用任何安全過濾，
+		// 這些請求將直接傳遞給Spring MVC或其他處理器進行處理。
+		web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
+	}
+
 }
